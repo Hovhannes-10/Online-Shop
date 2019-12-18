a6a4192b569c3bbb15b368647b82193d274a30eb
Hovhannes-10 <hovodanielyan@gmail.com>
2019-12-18 15:47:08 +0400

    Updates

---
+++ precache-manifest.7439ca31f982b7c66ffcff137d8e29b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc2690258a4d3b94a35a57f09277b19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ab768f6d7034026c564",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7720424fe45a0af45186",
+    "url": "/static/css/main.acaebd35.chunk.css"
+  },
+  {
+    "revision": "0ab768f6d7034026c564",
+    "url": "/static/js/2.11b98a09.chunk.js"
+  },
+  {
+    "revision": "7720424fe45a0af45186",
+    "url": "/static/js/main.f9e95cce.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
