7b229defabbdaa6d728d3e470a75ae73fcba66c1
Hovhannes-10 <hovodanielyan@gmail.com>
2019-12-21 00:22:21 +0400

    version 1.0.0

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const fileUpload =  require('express-fileupload')
 const mongoose = require("mongoose");
-const config = require("config")
+const config = require("config");
+const path = require("path")
 const app = express();
 
 app.use(express.json());
@@ -13,10 +14,16 @@ mongoose.connect(db, { useNewUrlParser: true,useUnifiedTopology: true , useCreat
 .then( () => console.log("MongoDB connected..."))
 .catch(err => console.log(err));
 
-app.use('/api/items', require('./routes/api/items'))
-app.use('/api/user', require('./routes/api/user'))
-app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+    app.get('*' , (req, res) => {
+        res.sendFile(path.resolve(__dirname , 'client','build','index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
